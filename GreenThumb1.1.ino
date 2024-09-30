6b7b4932472875c913e565a7d2578699918e8a86
robertonava08 <robtop378@gmail.com>
2024-09-30 09:37:06 -0500

    Added a prototype relay function

--- GreenThumb1.1.ino
+++ GreenThumb1.1.ino
@@ -1,5 +1,5 @@
 
-
+#define relay_pin 4
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,6 +7,7 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(A0,INPUT_PULLUP);
+  pinMode(relay_pin, OUTPUT);
 
 
 }
@@ -25,6 +26,8 @@ void loop() {
    // delays it .5 seconds will probably change later when full circuit is functioning
    delay(500);
 
+   // Call the relay module
+   relay();
 
 }
 
@@ -33,8 +36,18 @@ int readSensor() {
 
   int sensorValue = analogRead(A0);  // Read the analog value from sensor
 
-  int outputValue = map(sensorValue, 0, 1023, 0, 100); // Should map values from 0 to 100 but need to double check
+  int outputValue = map(sensorValue, 0, 1023, 100, 0); // Should map values from 0 to 100 but need to double check
 
   return outputValue;   // Return analog moisture value
+
+}
+
+// Prototype relay function 
+void relay(){
+
+  digitalWrite(relay_pin, LOW);
+  delay(1000);
+  digitalWrite(relay_pin, HIGH);
+  delay(1000);
 
 }
