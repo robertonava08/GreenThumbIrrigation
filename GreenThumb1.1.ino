1aeda6b39f533d6ea21e4525ef67435fde64e06b
robertonava08 <robtop378@gmail.com>
2024-10-12 13:46:30 -0500

    Added new content to README, fixed mapping issues, and added code for water pump to turn on during certain conditions.

--- GreenThumb1.1.ino
+++ GreenThumb1.1.ino
@@ -15,21 +15,26 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+   // Calls the readSensor() function and prints the returned magnitude value 
+   int soil_mouisture = readSensor();
 
    // Prints what the soil mouisture sensor is every .5 seconds
    Serial.print("Soil Moisture Level: ");
-
-   // Calls the readSensor() function and prints the returned magnitude value 
-   int soil_mouisture = readSensor();
-   Serial.println(soil_mouisture);
-   Serial.print("%");
+   Serial.print(soil_mouisture);
+   Serial.println("%");
 
    // delays it .5 seconds will probably change later when full circuit is functioning
    delay(500);
 
+
+   // If statement to call relay module if the SML reaches a certain level (we will use 35 for prototyping)
+   if(soil_mouisture <= 35){
+
    // Call the relay module
    relay();
 
+   }
+   
 }
 
 //  This function returns the analog data to calling function (the moisture level from 0 - 100 )
@@ -37,7 +42,15 @@ int readSensor() {
 
   int sensorValue = analogRead(A0);  // Read the analog value from sensor
 
-  int outputValue = map(sensorValue, 0, 1023, 100, 0); // Should map values from 0 to 100 but need to double check
+  // Print the sensor value to see what analog reads raw
+  Serial.print("Raw Sensor Value: ");
+  Serial.println(sensorValue);
+
+
+  int outputValue = map(sensorValue, 299, 707, 100, 0); // Calibrated the map function because the minimum values it reads is 299 and max is 707
+
+  // Constrain the output to be within 0-100 range (The actual values go a couple under and over 0 and 100 but that is negligible)
+  outputValue = constrain(outputValue, 0, 100);
 
   return outputValue;   // Return analog moisture value
 
@@ -46,9 +59,9 @@ int readSensor() {
 // Prototype relay function 
 void relay(){
 
-  digitalWrite(relay_pin, LOW);
-  delay(300);
-  digitalWrite(relay_pin, HIGH);
-  delay(300);
+  digitalWrite(relay_pin, LOW);    // Turn on the relay (close the circuit to the pump
+  delay(5000);                    // Wait for 5 seconds (the pump runs)
+  digitalWrite(relay_pin, HIGH);  // Turn off the relay (open the circuit to the pump)
+  //delay(1000);                    // Wait for 1 second (no pump operation) 
 
 }
--- README.md
+++ README.md
@@ -15,15 +15,8 @@ Smart Irrigation System Using an ESP8266 for Automated Plant Watering
           • 09/05/24 Not really an issue but need to start investigating how to make the app the interact with the system after it is done
 
 
-
-
-
-
-
-
-
-
-
+          • 09/12/24 The map function is not outputting the values correctly so I need to find the values to calibrate the SMS
+          • 09/12/24 Need to write the code so that the water pump only turns on when the soil moisture level (SML) reaches a certain level
 
 
 
@@ -31,6 +24,11 @@ Smart Irrigation System Using an ESP8266 for Automated Plant Watering
           
           • 09/05/24 Figured out why I couldnt upload code ( I needed to restart the ESP)
           • 09/05/24 Got the whole prototype system running now I can start making code for real application
+
+          • 09/12/24 Printed the raw values that the analog pin is reading and used that to calibrate the SMS (299-707)
+          • 09/12/24 The values still go a little over and under 0 and 100 but it is so small that it is negligible to consider
+          • 09/12/24 Wrote the code so that the water pump only turns on when the SML reaches at or below 35 (will test on real soil sample next time)
+
 
     
 
